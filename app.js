801be0ad82a11cad7097c48a963b136a860be4e4
william kung <wkung42@gmail.com>
2022-12-15 16:01:19 -0700

    adding some sample code

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+app.get("/browse", (req, res) => {
+  res.render("browse")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
