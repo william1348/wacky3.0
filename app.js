610b276b2ffd1b86baf52a8eaf93de9cb87fda90
william kung <wkung42@gmail.com>
2022-12-15 18:14:50 -0700

    getting mongodb working

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express')
 const path = require('path')
+const { MongoClient } = require("mongodb");
 const app = express()
+const util = require('util')
 const port = 3000
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index")
+var dbUrl ="mongodb+srv://admin:ilovemillie123@project.cib5r.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(dbUrl, (err, client) => {
+    console.log('we connected');
+	if (err) return console.log(err)
+	db = client.db('project') 
+	
+
+	app.set("view engine", "ejs");
+	app.use(express.static(path.join(__dirname, "public")));
+
+	app.get("/", (req, res) => {
+
+	db.collection("categories").find({}).toArray(function(error, result) {
+	    	if (err) throw err;	
+		//console.log(util.inspect(result))
+			res.render("index", { categories: result})
+		});
+	});
+
+	  
+
+	app.get("/browse", (req, res) => {
+	  res.render("browse")
+	});
+
+	app.listen(port, () => {
+	  console.log(`Example app listening on port ${port}`)
+	})
+
+
 });
-
-app.get("/browse", (req, res) => {
-  res.render("browse")
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
